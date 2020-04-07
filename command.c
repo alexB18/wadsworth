2b917d52e4c54fc51e32ac337c9c17af80b43eb8
alexB18 <abrown15@uoregon.edu>
2020-04-07 12:33:10 -0700

    Added command.h file and filled in command.c with skeletons of required functions detailed in command.h

---
+++ command.c
@@ -0,0 +1,40 @@
+#include "command.h"
+
+/*for the ls command*/
+void listDir(){
+}
+
+/*for the pwd command*/
+void showCurrentDir(){
+
+}
+
+/*for the mkdir command*/
+void makeDir(char *dirName){
+
+}
+
+/*for the cd command*/
+void changeDir(char *dirName){
+    
+}
+
+/*for the cp command*/
+void copyFile(char *sourcePath, char *destinationPath){
+
+}
+
+/*for the mv command*/
+void moveFile(char *sourcePath, char *destinationPath){
+
+}
+
+/*for the rm command*/
+void deleteFile(char *filename){
+
+}
+
+/*for the cat command*/
+void displayFile(char *filename){
+
+}
