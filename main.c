2c6b729472fd547cdcbb594c8bde3123869d8179
alexB18 <abrown15@uoregon.edu>
2020-04-19 00:00:04 -0700

    Boy oh boy is this a big ol commit. I reworked the entire tokenization loop so that it accepts commands in the form <cmd> <param1> <param2> ; <cmd2> ... . To do this, I modified the execUnixCmd function to take an array of pointers and an integer describing the number of arguments. I also defined a global array of strings called VALID_COMMANDS as well as a global int called NUM_OF_COMMANDS which I could use to check if incoming commands are valid. RIP

--- main.c
+++ main.c
@@ -21,6 +21,9 @@
 // 0 -> interactive mode
 // 1 -> file mode
 int MODE;
+// Pointer which will hold an array of all valid commands
+char** VALID_COMMANDS;
+int NUM_OF_COMMANDS = 8;
 
 /*--------------------------- Helper Functions ------------------------------*/
 
@@ -74,35 +77,87 @@ int usage(int argc, char** argv)
 	}
 }
 
-void execUnixCmd(char* command){
-	if(strcmp(command, "lfcat") == 0){
-		fprintf(stdout, "%s not yet implemented in command.c\n", command);
+int isValidCommand(char* command){
 
-	} else if(strcmp(command, "ls") == 0){
-		//fprintf(stdout, "%s not yet implemented in command.c\n", command);
-		listDir();
+	int result = 0;
+
+	//compare command with every command in VALID_COMMANDS
+	for(int i = 0; i < NUM_OF_COMMANDS; i++){
 		
-	} else if(strcmp(command, "pwd") == 0){
-		//fprintf(stdout, "%s not yet implemented in command.c\n", command);
-		showCurrentDir();
-	
-	} else if(strcmp(command, "mkdir") == 0){
-		fprintf(stdout, "%s not yet implemented in command.c\n", command);
-	
-	} else if(strcmp(command, "cd") == 0){
-		fprintf(stdout, "%s not yet implemented in command.c\n", command);
-	
-	} else if(strcmp(command, "cp") == 0){
-		fprintf(stdout, "%s not yet implemented in command.c\n", command);
-	
-	} else if(strcmp(command, "mv") == 0){
-		fprintf(stdout, "%s not yet implemented in command.c\n", command);
-	
-	} else if(strcmp(command, "rm") == 0){
-		fprintf(stdout, "%s not yet implemented in command.c\n", command);
+		// If we have any kind of match, set result to 1 and break
+		if(strcmp(VALID_COMMANDS[i], command) == 0){
+			result = 1;
+			break;
+		}
+	}
 
-	} else if(strcmp(command, "cat") == 0){
-		fprintf(stdout, "%s not yet implemented in command.c\n", command);
+	return result;
+}
+
+void execUnixCmd(char** command, int args){
+	//fprintf(stdout, "args: %i\n", args);
+
+	// If args == 0, then the user likely entered "exit"
+	if(args == 0){
+		return;
+		
+	} else if(strcmp(command[0], "ls") == 0){
+		//fprintf(stdout, "%s not yet implemented in command.c\n", command);
+		if(args != 1){
+			fprintf(stderr, "Error! Unsupported parameters for command: %s\n", command[0]);
+		} else {
+			listDir();
+		}
+		
+	} else if(strcmp(command[0], "pwd") == 0){
+		//fprintf(stdout, "%s not yet implemented in command.c\n", command);
+		if(args != 1){
+			fprintf(stderr, "Error! Unsupported parameters for command: %s\n", command[0]);
+		} else {
+			showCurrentDir();
+		}
+	
+	} else if(strcmp(command[0], "mkdir") == 0){
+		if(args != 2){
+			fprintf(stderr, "Error! Unsupported parameters for command: %s\n", command[0]);
+		} else {
+			fprintf(stdout, "%s not yet implemented in command.c\n", command[0]);
+		}
+
+	} else if(strcmp(command[0], "cd") == 0){
+		if(args != 2){
+			fprintf(stderr, "Error! Unsupported parameters for command: %s\n", command[0]);
+		} else {
+			fprintf(stdout, "%s not yet implemented in command.c\n", command[0]);
+		}
+	
+	} else if(strcmp(command[0], "cp") == 0){
+		if(args != 3){
+			fprintf(stderr, "Error! Unsupported parameters for command: %s\n", command[0]);
+		} else {
+			fprintf(stdout, "%s not yet implemented in command.c\n", command[0]);
+		}
+	
+	} else if(strcmp(command[0], "mv") == 0){
+		if(args != 3){
+			fprintf(stderr, "Error! Unsupported parameters for command: %s\n", command[0]);
+		} else {
+			fprintf(stdout, "%s not yet implemented in command.c\n", command[0]);
+		}
+	
+	} else if(strcmp(command[0], "rm") == 0){
+		if(args != 2){
+			fprintf(stderr, "Error! Unsupported parameters for command: %s\n", command[0]);
+		} else {
+			fprintf(stdout, "%s not yet implemented in command.c\n", command[0]);
+		}
+
+	} else if(strcmp(command[0], "cat") == 0){
+		if(args != 2){
+			fprintf(stderr, "Error! Unsupported parameters for command: %s\n", command[0]);
+		} else {
+			fprintf(stdout, "%s not yet implemented in command.c\n", command[0]);
+		}
 
 	} else {
 		fprintf(stdout, "Unrecognized command.\n");
@@ -116,19 +171,31 @@ int main(int argc, char** argv) {
 
 	// Global Variable Initializations
 	MODE = usage(argc, argv);
+	VALID_COMMANDS = (char**) malloc(NUM_OF_COMMANDS * sizeof(char*));
+	VALID_COMMANDS[0] = "ls";
+	VALID_COMMANDS[1] = "pwd";
+	VALID_COMMANDS[2] = "mkdir";
+	VALID_COMMANDS[3] = "cd";
+	VALID_COMMANDS[4] = "cp";
+	VALID_COMMANDS[5] = "mv";
+	VALID_COMMANDS[6] = "rm";
+	VALID_COMMANDS[7] = "cat";
 	/* DEBUG
 	fprintf(stdout, "Mode: %i\n", MODE);
 	*/
 
 	/* Main Function Variables */
 	int CONTINUE = 1;
-	int numLineCharacters;
-	char *inBufferPtr, *token, **savePtr;
+	int TOKEN_ERROR;
+	int numLineCharacters, i;
+	char *inBufferPtr, *token, **savePtr, **tokens;
 	size_t bufferSize = 80;
+	size_t tokenBuffer = 3;
 
-	/* Allocate memory for the input inBufferPtr. and savePtr */
+	/* Allocate memory for the input inBufferPtr, savePtr, and tokens */
 	inBufferPtr = (char*) malloc(bufferSize * sizeof(char));
 	savePtr = (char**) malloc(sizeof(char*));
+	tokens = (char**) malloc(tokenBuffer * sizeof(char*));
 
 	/* Initialize inFilePointer and outFilePointer to null by default */
 	FILE *inFilePointer, *outFilePointer;	
@@ -162,9 +229,6 @@ int main(int argc, char** argv) {
 		}
 
 	}
-	
-		 // Remove the newline at the end of the input string.
-	 	//Hint: use strcspn(2) to find where the newline is then assign '\0' there.
 
 	// Main run cycle
 	do
@@ -176,7 +240,7 @@ int main(int argc, char** argv) {
 
 
 		// Strip newline at the end of the input string
-		inBufferPtr[strlen(inBufferPtr) - 1] = 0;
+		inBufferPtr[strlen(inBufferPtr) - 1] = '\0';
 
 		// Save copy of inBufferPointer
 		*savePtr = inBufferPtr;
@@ -186,15 +250,12 @@ int main(int argc, char** argv) {
 		}		
 	
 		/* Tokenize and process the input string. Remember there can be multiple
-		calls to lfcat. i.e. lfcat ; lfcat <-- valid
-			If the command is either 'exit' or 'lfcat' then do the approp. things.
-		Note: you do not need to explicitly use an if-else clause here. For
-				instance, you could use a string-int map and a switch statement
-					or use a function that compares the input token to an array of
-					commands, etc. But anything that gets the job done gets full points so
-					think creatively. ^.^  Don't forget to display the error messages
-					seen in the lab discription*/
+		calls to lfcat. i.e. lfcat ; lfcat <-- valid*/
+		i = 0;
 		while ((token = strtok_r(*savePtr, " ", savePtr))){
+			// Set error flag back to 0
+			TOKEN_ERROR = 0;
+
 			
 			// If the user entered <exit> then exit both loops
 			if(strcmp(token, "exit") == 0){
@@ -202,13 +263,54 @@ int main(int argc, char** argv) {
 					break;	
 			
 			// If the user entered nothing, then simply repeat loop & ask for input again
-			} else if(strcmp(token, "\n") == 0){
+			} else if(strcmp(token, "\0") == 0){
+				fprintf(stdout, "newline\n");
 				break;
-			
+
+			// If the token is control code, we need to check to make sure it isn't at the end of the line
+			} else if(strcmp(token, ";") == 0){
+
+				//If tokens isn't empty, then we need to call execUnixCmd on it
+				execUnixCmd(tokens, i);
+
+				/* Then we need to clear tokens for the next potential batch of parameters,
+				reset i to 0, and continue the tokenization loop*/
+				free(tokens);
+				tokens = (char**) malloc(tokenBuffer * sizeof(char*));
+				i = 0;
+				continue;
+
+			// If the current token is a valid command, we need to make sure it's the only one
+			} else if(isValidCommand(token)){
+
+				if(i > 0){
+					fprintf(stderr, "Error! Incorrect syntax. No control code found.\n");
+					TOKEN_ERROR = 1;
+					break;
+				}
+
+				// If this command is the first valid one, add it to tokens
+				tokens[i] = token;
+
+			// Else the token might be a parameter and we need to make sure it's not the first argument 
 			} else{
-				execUnixCmd(token);
-			}		
+				
+				if(i == 0){
+					fprintf(stderr, "Error! Unrecognized command: %s \n", token);
+					TOKEN_ERROR = 1;
+					break;
+				}
+
+				tokens[i] = token;
+			}
+
+			i++;		
 		}
+
+		if( (tokens[0] != NULL) && (strcmp(tokens[0], "exit") != 0) && (!TOKEN_ERROR) ) {
+			execUnixCmd(tokens, i);
+		}
+		//free(tokens);
 
 	} while((numLineCharacters != -1) && (CONTINUE));
 
@@ -221,6 +323,8 @@ int main(int argc, char** argv) {
 
 	free(inBufferPtr);
 	free(savePtr);
+	free(tokens);
+	free(VALID_COMMANDS);
 
 	return 0;
 }
